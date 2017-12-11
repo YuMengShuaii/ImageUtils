641370bf6ed2080edcfa5cbcdbca249bb1d730e1
yumengshuai <yumengshuai@gmail.com>
2017-12-11 16:58:39 +0800

    更新项目目录，提升性能

--- imagepluin/src/main/java/com/enation/javashop/imagepluin/utils/ImageUtils.java
+++ imagepluin/src/main/java/com/enation/javashop/imagepluin/utils/ImageUtils.java
@@ -2,6 +2,8 @@ package com.enation.javashop.imagepluin.utils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.WorkerThread;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -16,38 +18,52 @@ public class ImageUtils {
     /**
      * 获取本地图片Bitmap
      * @param paramString
-     * @return
      */
-    @WorkerThread
-    public static Bitmap getLoacalBitmap(String paramString)
+    public static void getLoacalBitmap(final String paramString, final ImageListener<Bitmap> listener)
     {
-        Bitmap localBitmap;
-        localBitmap = BitmapFactory.decodeFile(paramString);
-        return localBitmap;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap localBitmap = BitmapFactory.decodeFile(paramString);
+                if (localBitmap == null){
+                    listener.failed();
+                }else{
+                    listener.success(localBitmap);
+                }
+            }
+        }).start();
     }
 
     /**
      * Bitmap本地化
      * @param paramBitmap  需要本地化的BitMap
      * @param paramString  需要序列化的路径
-     * @throws IOException IO异常
      */
-    @WorkerThread
-    public static void bitmapToSdCard(Bitmap paramBitmap, String paramString,ImageListener listener)
-            throws IOException
-    {
-        File localFile1 = new File(paramString);
-        if (!(localFile1.exists()))
-            localFile1.mkdirs();
-        File localFile2 = new File(paramString, System.currentTimeMillis() + ".jpg");
-        FileOutputStream localFileOutputStream = new FileOutputStream(localFile2);
-        paramBitmap.compress(Bitmap.CompressFormat.JPEG, 100, localFileOutputStream);
-        localFileOutputStream.flush();
-        if (localFileOutputStream != null)
-            localFileOutputStream.close();
-        listener.success(localFile2);
+    public static void bitmapToSdCard(final Bitmap paramBitmap, final String paramString, final ImageListener<File> listener) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    File localFile1 = new File(paramString);
+                    if (!(localFile1.exists())) localFile1.mkdirs();
+                    File localFile2 = new File(paramString, System.currentTimeMillis() + ".jpg");
+                    FileOutputStream localFileOutputStream = new FileOutputStream(localFile2);
+                    paramBitmap.compress(Bitmap.CompressFormat.JPEG, 100, localFileOutputStream);
+                    localFileOutputStream.flush();
+                    localFileOutputStream.close();
+                    if (localFileOutputStream != null){
+                        listener.success(localFile2);
+                    }else{
+                        listener.failed();
+                    }
+                } catch (IOException e) {
+                    listener.failed();
+                }
+            }
+        }).start();
     }
-    interface ImageListener{
-        void success(File path);
+    interface ImageListener<T>{
+        void success(T result);
+        void failed();
     }
 }
